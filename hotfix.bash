fa09e98cf96f8d8dd8624a1287752778c485a3db
Blake Owens <blake.owens@10security.com>
2022-09-15 14:53:02 -0500

    Removed questionnaire hotfix

--- hotfix.bash
+++ hotfix.bash
@@ -1,5 +1,1 @@
 #!/bin/bash
-
-# Questionnaires Hotfix
-cp -r ./docs/questionnaires/* ./django-DefectDojo/docs/content/en/usage/
-cp ./images/questionnaires/* ./django-DefectDojo/docs/static/images/
